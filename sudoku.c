48378539a11daf765d9fb0f0c9b81db7b9b7b04f
Your Name <you@example.com>
2022-05-15 21:08:11 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,33 +43,102 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int is_valid(Node* n){
+int is_valid_fila(int fil, Node* n){
+  int valoresAux[9];
+  for( int i = 0; i < 10; i++){
+    valoresAux[i] = 0;
+  }
 
+  for( int i = 0; i < 9; i++){
+    if( n->sudo[fil][i] != 0){
+      if( valoresAux[ n->sudo[fil][i] - 1] != 1){
+        valoresAux[ n->sudo[fil][i] - 1] = 1;
+      }
+      else{
+        return 0;
+      }
+    }
+  }
+
+  return 1;
+}
+
+int is_valid_columna(int col, Node* n){
+  int valoresAux[9];
+  for( int i = 0; i < 10; i++){
+    valoresAux[i] = 0;
+  }
+
+  for( int i = 0; i < 9; i++){
+    if( n->sudo[i][col] != 0){
+      if( valoresAux[ n->sudo[i][col] - 1] != 1){
+        valoresAux[ n->sudo[i][col] - 1] = 1;
+      }
+      else{
+        return 0;
+      }
+    }
+  }
+
+  return 1;
+}
+
+int is_valid_submatrix(int subM, Node* n){
+  int valoresAux[9];
+  for( int i = 0; i < 10; i++){
+    valoresAux[i] = 0;
+  }
+ 
+  for(int p = 0;p < 9; p++){
+      int i = 3*(subM / 3) + (p / 3) ;
+      int j = 3*(subM % 3) + (p % 3) ;
+      if( n->sudo[i][j] != 0){
+        if( valoresAux[ n->sudo[i][j] - 1] != 1){
+        valoresAux[ n->sudo[i][j] - 1] = 1;
+        }
+        else{
+          return 0;
+        }
+      }
+      
+  }
+
+  return 1;
+}
+
+int is_valid(Node* n){
+  
+  for( int i = 0; i < 9; i++){
+    if ( !is_valid_fila(i, n) || !is_valid_columna(i, n) || !is_valid_submatrix(i, n) ){
+      return 0;
+    }
+  }
     return 1;
 }
 
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
-    Node *newNode;
-    size_t cont, i, j;
 
-    for (i = 0; i < 9; i++) {
-      for (j = 0; j < 9; j++) {
-        if (n->sudo[i][j] == 0) {
-          for (cont = 1; cont <= 9; cont++) {
-            newNode = copy(n);
-            newNode->sudo[i][j] = cont;
-            pushBack(list, newNode);
-            return list;
+    for(int i = 0; i < 9; i++){
+      for(int j = 0; j < 9; j++){
+
+        if (n->sudo[i][j] == 0){
+
+          for ( int k = 1; k < 10; k++){
+            Node* nuevoEstado = copy( n );
+            nuevoEstado->sudo[i][j] = k;
+            if( is_valid(nuevoEstado) ){
+              pushBack( list , nuevoEstado );
+            }
           }
+          return list;
+
         }
-        newNode = copy(n);
-        pushBack(list, n);
+
       }
     }
-
-    return list;
+  return list;
 }
 
 
