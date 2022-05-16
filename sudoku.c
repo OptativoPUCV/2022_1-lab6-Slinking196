76c0db9acfb6be497510723ed1876219be86167e
Your Name <you@example.com>
2022-05-15 22:06:40 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -96,14 +96,14 @@ int is_valid(Node* n){
     size_t cont;
 
     for (cont = 0; cont < 9; cont++) {
-      if (repetidosPorFila(cont, n) != 0) return 0;
+      if (repetidosPorFila(cont, n) == 0) return 1;
       printf("xd1\n");
-      if (repetidosPorColumna(cont, n) != 0) return 0;
+      if (repetidosPorColumna(cont, n) == 0) return 1;
       printf("xd2\n");
-      if (repetidosPorSubMatriz(cont, n) != 0) return 0;
+      if (repetidosPorSubMatriz(cont, n) == 0) return 1;
       printf("xd3\n");
     }
-    return 1;
+    return 0;
 }
 
 
