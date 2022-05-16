0fbdfccee566dcaf2133804dc8a88eddc8a0be84
Your Name <you@example.com>
2022-05-15 22:47:38 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,8 +51,11 @@ int repetidosPorColumna(size_t c, Node *n) {
       if (n->sudo[c][i] == 0) continue;
       for (j = i + 1; j < 9; j++) {
         if (n->sudo[j][c] == 0) continue;
-        printf("a = %d y b = %d\n", n->sudo[c][i], n->sudo[c][j]);
-        //if(n->sudo[c][i] == n->sudo[i][c]) return 0;
+        printf("a = %d y b = %d\n", n->sudo[c][i], n->sudo[j][c]);
+        if(n->sudo[c][i] == n->sudo[i][c]) { 
+          printf("xd");
+          return 0;
+        }       
       }
     }
 
