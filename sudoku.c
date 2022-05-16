9bb084d399dbfb4559af7c097a9de30ae71d45e3
Your Name <you@example.com>
2022-05-15 22:19:46 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -110,6 +110,7 @@ int is_valid(Node* n){
     mostrarMatriz(n);
     for (cont = 0; cont < 9; cont++) {
       if (repetidosPorFila(cont, n) == 0 && repetidosPorColumna(cont, n) == 0 && repetidosPorSubMatriz(cont, n) == 0) return 0;
+      printf("%d\n", cont);
     }
     return 1;
 }
