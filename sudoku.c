c6a9483e67ac3a2ac598c65a546bb26fd66ba0a5
Your Name <you@example.com>
2022-05-15 23:02:34 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -101,7 +101,7 @@ int repetidosPorSubMatriz(size_t k, Node *n) {
 int is_valid(Node* n){
     size_t cont;
     for (cont = 0; cont < 9; cont++) {
-      if (repetidosPorFila(cont, n) == 0 && repetidosPorColumna(cont, n) == 0 && repetidosPorSubMatriz(cont, n) == 0) return 0;
+      if (repetidosPorFila(cont, n) == 0 || repetidosPorColumna(cont, n) == 0 || repetidosPorSubMatriz(cont, n) == 0) return 0;
       /*if (repetidosPorFila(cont, n) == 0) return 0;
       if (repetidosPorColumna(cont, n) == 0) return 0;
       if (repetidosPorSubMatriz(cont, n) == 0) return 0;*/
