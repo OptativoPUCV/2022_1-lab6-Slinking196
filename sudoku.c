5713e5158a56e6a6dc1fd1c91343f8d2627db12c
Your Name <you@example.com>
2022-05-15 22:03:48 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -77,10 +77,10 @@ int repetidosPorSubMatriz(size_t k, Node *n) {
     int i = 3 * (k / 3) + (p / 3);
     int j = 3 * (k % 3) + (p % 3);
     vector[p] = n->sudo[i][j];
-    printf("%d ", n->sudo[i][j]);
+    //printf("%d ", n->sudo[i][j]);
    
     if(p % 3 == 2) { 
-      printf("\n");
+      //printf("\n");
     }
   }
   
